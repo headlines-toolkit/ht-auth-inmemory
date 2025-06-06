e1e39f5399cd293aac7109c0af123896dd9d6ede
fulleni <fulleni@hotmail.com>
2025-06-06 05:20:15 +0100

    chore: init

---
+++ lib/src/ht_auth_inmemory.dart
@@ -0,0 +1,7 @@
+/// {@template ht_auth_inmemory}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtAuthInmemory {
+  /// {@macro ht_auth_inmemory}
+  const HtAuthInmemory();
+}
---
+++ test/src/ht_auth_inmemory_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_auth_inmemory/ht_auth_inmemory.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtAuthInmemory', () {
+    test('can be instantiated', () {
+      expect(HtAuthInmemory(), isNotNull);
+    });
+  });
+}
